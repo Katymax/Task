06027114c10462afed8f01afe34a3a482b807025
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-11 17:45:01 +0300

    Create 1293 Eniya.cs

---
+++ 1293 Eniya.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Eniya
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] x = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(x[0]) * int.Parse(x[1]) * int.Parse(x[2]) * 2);
+        }
+    }
+}
