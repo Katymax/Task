779e4eb05fb69b90181f949ad042378afcf3e36c
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-11 17:31:58 +0300

    Create 1000 A+B Problem.cs

---
+++ 1000 A+B Problem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int a, b;
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
