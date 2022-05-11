9c0b476815214cd5a84783caf11c8539d1b8717f
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-11 17:50:49 +0300

    Create 1264 Workdays.cs

---
+++ 1264 Workdays.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Workdays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x = Console.ReadLine().Split(' ');
+            long n = int.Parse(x[0]);
+            long m = int.Parse(x[1]);
+            Console.WriteLine(n * (m + 1));
+
+        }
+    }
+}
