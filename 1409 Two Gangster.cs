1a856062c59d45fff6f69ffdd0a1e4768a695288
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-11 18:22:09 +0300

    Create 1409 Two Gangster.cs

---
+++ 1409 Two Gangster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gangster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x = (Console.ReadLine()).Split(' ');
+            int a = Int32.Parse(x[0]);
+            int b = Int32.Parse(x[1]);
+            Console.WriteLine((b - 1) + " " + (a - 1));
+        }
+    }
+}
