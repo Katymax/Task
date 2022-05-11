46c8080d124d484f8a27234ae51b6a65d466daa5
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-11 17:49:34 +0300

    Create 1877 Bicycle Codes.cs

---
+++ 1877 Bicycle Codes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bicycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a % 2 == 0 || b % 2 == 1)
+                Console.WriteLine("yes");
+            else if (a % 2 != 0 || b % 2 == 1)
+                Console.WriteLine("no");
+        }
+    }
+}
