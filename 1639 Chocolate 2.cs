829bd3e8aadeb5a217bdd92d36ebfb1c98456d0b
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-18 16:33:03 +0300

    Create 1639 Chocolate 2.cs

---
+++ 1639 Chocolate 2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x = (Console.ReadLine()).Split(' ');
+            int m = Int32.Parse(x[0]);
+            int n = Int32.Parse(x[1]);
+
+            if ((m * n) % 2 == 0)
+                Console.WriteLine("[:=[first]");
+            else if ((m * n) % 2 != 0)
+                Console.WriteLine("[second]=:]");
+        }
+    }          
+}
