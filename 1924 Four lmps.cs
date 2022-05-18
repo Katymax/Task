e3ed08ef8c40cdd30fb02f207467af5fcf433b6b
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-18 16:24:23 +0300

    36_1924 Four lmps

---
+++ 1924 Four lmps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lmps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Int32.Parse(Console.ReadLine());
+            if (((x * (x + 1)) / 2) % 2 == 0)  
+                Console.WriteLine("black"); 
+            else 
+                Console.WriteLine("grimy"); 
+        }
+    }
+}
