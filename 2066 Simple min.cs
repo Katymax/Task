a3f1d1ca8256288a77067da6cdc4a78b814d7142
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-18 16:47:42 +0300

    Create 2066 Simple min.cs

---
+++ 2066 Simple min.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int z = 0;int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            x = a; z = b; y = c;
+            int q = x - y * z;
+            int i = x - z - y;
+            Console.WriteLine(Math.Min(q,i));
+        }
+    }
+}
