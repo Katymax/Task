bef054558a7f7c0ac2fd3adba454460d1aa6e9a3
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-18 17:02:07 +0300

    Create 1820 Ural Steaks.cs

---
+++ 1820 Ural Steaks.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Ural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x = Console.ReadLine().Split(' '); 
+            int n = int.Parse(x[0]);  
+            int k = int.Parse(x[1]);   
+            if (n <= k)
+                Console.WriteLine(2); 
+            else if (n >= k)
+            Console.WriteLine((2 * n - 1) / k + 1);
+        }
+    }
+}
