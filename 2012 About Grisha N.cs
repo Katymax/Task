e6f93fd28cbc61b185c192b974d4ddc8cc5c1482
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-18 17:10:53 +0300

    Create 2012 About Grisha N.cs

---
+++ 2012 About Grisha N.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Grisha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine().Trim());
+            Console.WriteLine((x >= 7)? "YES" : "NO");
+            Console.ReadLine(); 
+        }
+    }
+}
