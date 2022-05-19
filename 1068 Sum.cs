25e3a868bcabc5dc5dc863998edb8b9abafcc54d
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-19 16:59:28 +0300

    Create 1068 Sum.cs

---
+++ 1068 Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int x = 0;
+            if (N < 0 && N >= -10000)
+            {
+                for (int i = N; i <= 1; i++)
+                    x += i;
+            }
+            else if (N > 0 && N <= 10000)
+            {
+                for (int i = 1; i <= N; i++)
+                    x += i;
+            }
+            else 
+                x = 1;
+            Console.WriteLine(x);
+        }
+    }
+}
