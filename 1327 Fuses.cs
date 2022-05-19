69261e54f2efd38a1752dcce5e1167fec0003694
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-19 20:25:01 +0300

    Create 1327 Fuses.cs

---
+++ 1327 Fuses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int X = (B - A + (A % 2) + (B % 2));
+            Console.WriteLine(X/2);
+
+        }
+    }
+}
