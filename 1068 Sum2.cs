11d84599efd4ba63d7f2ab366563e8652db7cc38
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-19 18:11:25 +0300

    Create 1068 Sum2.cs

---
+++ 1068 Sum2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            double sum = ((N > 0 ? N * N : 2 - N * N) + N) / 2;
+            Console.WriteLine(sum);
+        }
+    }
+}
