79f658927cae1238e0a158f9e68556505807d36e
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-20 17:58:19 +0300

    Create 2056 Scholarship.cs

---
+++ 2056 Scholarship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Scholardhip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            bool x = false;
+            bool y = true;
+
+            double q = 0;
+            for (int i = 0; i < N && !x; i++)
+            {
+                int z = int.Parse(Console.ReadLine());
+                if (z == 3) x = true;
+                q += z;
+                if (z < 5) y = false;
+            }
+
+            if (x) 
+                Console.WriteLine("None");
+            else if (y) 
+                Console.WriteLine("Named");
+            else if (q / N >= 4.5) 
+                Console.WriteLine("High");
+            else 
+                Console.WriteLine("Common");
+
+        }
+    }
+}
