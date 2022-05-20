7171ca1c1c20211227969932a7f958bd1f78eaae
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-20 18:37:08 +0300

    Create 1243 Divorse Of The Seven Dwarfs(не принимает).cs

---
+++ 1243 Divorse Of The Seven Dwarfs(не принимает).cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SevenDwarfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long x = long.Parse(Console.ReadLine());
+            long y = x % 7;
+
+            Console.WriteLine(y);
+
+        }
+    }
+}
