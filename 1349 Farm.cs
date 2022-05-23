d9022e744362b0ee590dff8bd00b62c8f2079193
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-23 13:32:21 +0300

    Create 1349 Farm.cs

---
+++ 1349 Farm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Farm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string a = "1 2 3";
+            string b = "3 4 5";
+
+            if (N != 1 && N != 2)
+            {
+                Console.WriteLine("-1");
+                return;
+            }
+
+            Console.WriteLine(N == 1 ? a : b);
+
+        }
+    }
+}
+
+
