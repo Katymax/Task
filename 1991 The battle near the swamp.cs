e0dfae7995782658a2ec6b17b398c12fd6905202
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-23 16:17:19 +0300

    Create 1991 The battle near the swamp.cs

---
+++ 1991 The battle near the swamp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] N = Console.ReadLine().Split(' ');
+            string[] S = Console.ReadLine().Split(' ');
+            int a = int.Parse(N[0]);
+            int b = int.Parse(N[1]);
+
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < a; i++)
+            {
+                int z = int.Parse(S[i]);
+                if (z > b)
+
+                    y += z - b;
+
+                else if (b > z)
+
+                    x += b - z;
+
+            }
+
+            Console.WriteLine( y + " " + x);
+
+        }
+    }
+}
