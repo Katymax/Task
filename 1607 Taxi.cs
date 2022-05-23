acf57f994dd4381c9b14cc02d12aae862937fe8b
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-23 12:13:59 +0300

    Create 1607 Taxi.cs

---
+++ 1607 Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] N = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(N[0]);
+            int y = int.Parse(N[1]);
+            int a = int.Parse(N[2]);
+            int b= int.Parse(N[3]);
+
+            while (x <= a)
+            {
+               x += y;
+
+                if (x > a)
+                {
+                    Console.WriteLine(a);
+                    return;
+                }
+
+                a -= b;
+            }
+
+            Console.WriteLine(x);
+        }
+    }
+}
+
