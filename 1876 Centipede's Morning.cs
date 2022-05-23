2d580c7f00b988d1b572e938de9f3493b19043ef
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-23 15:11:33 +0300

    Create 1876 Centipede's Morning.cs

---
+++ 1876 Centipede's Morning.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Morning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+            int a = int.Parse(s.Split()[0]);
+            int b = int.Parse(s.Split()[1]);
+            int N = 0;
+            int S = 0;
+
+            N = 40 + 2 * b;
+            S = 2 * a + 39;
+            if (N > S)
+                Console.WriteLine(N);
+
+            else if (N < S)
+
+            Console.WriteLine(S);
+
+        }
+    }
+}
+
