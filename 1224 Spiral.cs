614de1705344013da7e06a0162fd98641bdd51ac
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-24 17:10:37 +0300

    Create 1224 Spiral.cs

---
+++ 1224 Spiral.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Spiral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            long a = long.Parse(N.Split()[0]);
+            long b = long.Parse(N.Split()[1]);
+            long x = 0;
+            if (a > b) 
+                x = b + b - 1;
+            else 
+                x = (a - 1) * 2;
+            Console.WriteLine(x);
+
+        }
+    }
+}
+
