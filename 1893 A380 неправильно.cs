3e80108462761ea4a7fd53a5f015cf8fcf0ab476
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-25 16:16:23 +0300

    Create 1893 A380 неправильно.cs

---
+++ 1893 A380 неправильно.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace A380
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string N = Console.ReadLine();
+            char a = N[N.Length - 1];
+            int b= N[N.Length - 1];
+
+            if (b < 3)
+            {
+                if (a == 'A' | a == 'D') 
+                    Console.WriteLine("window");
+                else 
+                    Console.WriteLine("aisle");
+            }
+            else if (b < 21)
+            {
+                if (a == 'A' | a == 'F') 
+                    Console.WriteLine("window");
+                else 
+                    Console.WriteLine("aisle");
+            }
+            else
+            {
+                if (a == 'A' | a == 'K') 
+                    Console.WriteLine("window");
+                else if (a == 'C' | a == 'D' | a == 'G' | a == 'H')
+                    Console.WriteLine("aisle");
+                else 
+                    Console.WriteLine("neither");
+
+            }
+
+        }
+    }
+}
