bfe8b43ae723bbffa3afcfb47bced51870861c60
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-25 16:39:23 +0300

    Create 1638 Bookworm.cs

---
+++ 1638 Bookworm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bookworm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            int a = int.Parse(N.Split()[0]);
+            int b = int.Parse(N.Split()[1]);
+            int x = int.Parse(N.Split()[2]);
+            int y = int.Parse(N.Split()[3]);
+            
+
+            Console.WriteLine(Math.Abs((y - x ) * (2 * b + a)-a));
+        }
+    }
+}
