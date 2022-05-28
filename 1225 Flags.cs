519caf3543b186cbe7689a174a0900cc6c499b7b
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-28 05:22:50 +0300

    Create 1225 Flags.cs

---
+++ 1225 Flags.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Flags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+            long[] x = new long[N];
+            x[0] = 2;
+            x[1] = 2;
+            if (x.Length > 1) x[1] = 2;
+            for (int i = 2; i < x.Length; i++)
+                x[i] = x[i - 2] + x[i - 1];
+
+            Console.WriteLine(x[N - 1]);
+        }
+    }
+}
