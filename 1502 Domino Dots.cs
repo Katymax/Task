50148edc7ac31bb7230dc00642704bdcedc306c5
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-30 17:08:12 +0300

    Create 1502 Domino Dots.cs

---
+++ 1502 Domino Dots.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long x = long.Parse(Console.ReadLine());
+            long y = 0;
+            y = x * (x + 1) * (x + 2) / 2;
+            Console.WriteLine(y);
+
+        }
+    }
+}
