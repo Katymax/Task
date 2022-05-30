03cc2575b4af42e15ef8c3f77a570278e2b6dad5
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-30 20:32:21 +0300

    Create 1083 Factorials!!!.cs

---
+++ 1083 Factorials!!!.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] N = Console.ReadLine().Split(' ');
+            int x = int.Parse(N[0]);
+            int y = N[1].Length;
+            int z = 0;
+            int a = 1;
+            int b = 1;
+            while (b >= 1)
+            {
+                a *= b;
+                b = x - (z * y);
+                z++;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
