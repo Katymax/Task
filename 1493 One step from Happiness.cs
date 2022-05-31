6a6a42d5f7dc8101d0d34942410ea4eb70647dc9
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-31 16:55:16 +0300

    Create 1493 One step from Happiness.cs

---
+++ 1493 One step from Happiness.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OneStep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int x = 0;
+            int y = 0;
+            int c = 0;
+            x = N % 10;
+            for (int i = 0; i < 3; i++)
+            {
+                c += N % 10;
+                N = N / 10;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                y += N % 10;
+                N = N / 10;
+            }
+            if ((y > c) && (x == 9))
+                Console.WriteLine("No");
+            else if ((y < c) && (x == 0))
+                Console.WriteLine("No");
+            else
+                Console.WriteLine("Yes");
+        }
+    }
+}
