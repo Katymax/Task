5c615a53af1220f33deceebaf44aa46020407bc1
Katymax <104359769+Katymax@users.noreply.github.com>
2022-05-31 16:14:33 +0300

    Create 1787 Turn of MEGA.cs

---
+++ 1787 Turn of MEGA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MEGA
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] N = Console.ReadLine().Split(' ');
+            int x = int.Parse(N[0]);
+            int y = int.Parse(N[1]);
+            int a;
+            int b;
+            int c = 0;
+            N = Console.ReadLine().Split(' ');
+            
+            for (a = 0; a < y; a++)
+            {
+                b = int.Parse(N[a]);
+                c = Math.Max(0, c + b - x);
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
