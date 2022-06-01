b547ba4978be0c8865665761816fd792f0793202
Katymax <104359769+Katymax@users.noreply.github.com>
2022-06-01 14:04:11 +0300

    Create 1131 Copying.cs

---
+++ 1131 Copying.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Copying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            int a = int.Parse(N.Split()[0]);
+            int b = int.Parse(N.Split()[1]);
+            int x = 0;
+            int y = 1;
+
+            while (y < a)
+            {
+                if (y < b)
+                {
+                    ++x;
+                    y += Math.Min(y, b);
+                }
+                else
+                {  
+                    x += (a - y) / b;
+                    if ((a - y) % b != 0)
+                        ++x;
+                    break;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
