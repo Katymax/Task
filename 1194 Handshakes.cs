2b73995cc731a661a806320b07b6541acfd451cb
Katymax <104359769+Katymax@users.noreply.github.com>
2022-06-03 15:46:02 +0300

    Create 1194 Handshakes.cs

---
+++ 1194 Handshakes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Handshakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            int a = int.Parse(N.Split()[0]);
+            int b = int.Parse(N.Split()[1]);
+            Console.WriteLine((a - 1) * a / 2 - b);
+
+        }
+    }
+}
