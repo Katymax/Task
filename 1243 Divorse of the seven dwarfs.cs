204cef62e7f566319a5dbd9924549be37c2f5413
Katymax <104359769+Katymax@users.noreply.github.com>
2022-06-03 16:12:54 +0300

    Create 1243 Divorse of the seven dwarfs.cs

---
+++ 1243 Divorse of the seven dwarfs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dwarfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            int a = 0;
+            int b = 1;
+            for (int i = N.Length - 1; i >= 0; i--)
+            {
+                int x = N[i] - '0';
+                a += b * x;
+                b *= 3;
+                b %= 7;
+                a %= 7;
+            }
+            Console.WriteLine(a);
+
+        }
+    }
+}
