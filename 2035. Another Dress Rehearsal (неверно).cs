a99db18bd950093019a1ab631861344d3397810d
Katymax <104359769+Katymax@users.noreply.github.com>
2022-06-06 17:59:11 +0300

    Create 2035. Another Dress Rehearsal (неверно).cs

---
+++ 2035. Another Dress Rehearsal (неверно).cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Another
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string N = Console.ReadLine();
+            int a = int.Parse(N.Split()[0]);
+            int b = int.Parse(N.Split()[1]);
+            int c = int.Parse(N.Split()[2]);
+            if (a + b < c)
+            {
+                Console.WriteLine("impossible");
+            }
+            else if (a <= c)
+            {
+                Console.WriteLine(a + " " + (c - a));
+
+            }
+            else 
+            {
+                Console.WriteLine(c + " " + 0);
+            }
+
+        }
+    }
+}
